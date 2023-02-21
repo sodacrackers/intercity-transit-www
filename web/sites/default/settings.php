5e3209d83e7c0901f2575b7809be96bed2c7007f
Benji Fisher <benji.fisher@fruition.net>
2023-02-21 13:59:49 -0500

    ICTROUTES-41: Copy some settings from the fefault branch

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,4 +1,19 @@
 <?php
+/* Store the current Pantheon site environment */
+$current_env = $_ENV["PANTHEON_ENVIRONMENT"];
+
+if ($current_env != 'live'):
+  error_reporting(E_ALL);
+  ini_set('display_errors', TRUE);
+  ini_set('display_startup_errors', TRUE);
+  $config['system.logging']['error_level'] = 'verbose';
+endif;
+
+if ($current_env == 'live'):
+  $config['saml_sp_drupal_login.config']['force_saml_only'] = TRUE;
+else:
+  $config['saml_sp_drupal_login.config']['force_saml_only'] = FALSE;
+endif;
 
 /**
  * Load services definition file.
@@ -31,6 +46,20 @@ include __DIR__ . "/settings.pantheon.php";
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
+}
+
+/**
+ * Place the config directory outside of the Drupal root.
+ */
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
+
+
+$docuri = $_SERVER['REQUEST_URI'];
+if ((strpos($docuri, 'traveloptions/vanpoolandcarpool/currentvanpools/Pages/VanpoolDetails.aspx?itemId') !== false) &&
+  (php_sapi_name() != "cli")) {
+  $newloc = '/vanpool/join';
+  header('Location: http://intercitytransit.com' . $newloc);
+  exit();
 }
 
 // Automatically generated include for settings managed by ddev.
