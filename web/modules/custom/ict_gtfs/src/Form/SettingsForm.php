9fb727f31fc122568282e92ff91e52cabc0be8ae
Benji Fisher <benji.fisher@fruition.net>
2023-02-23 15:08:08 -0500

    ICTROUTES-40: Add configuration items to the settings form

--- web/modules/custom/ict_gtfs/src/Form/SettingsForm.php
+++ web/modules/custom/ict_gtfs/src/Form/SettingsForm.php
@@ -28,31 +28,48 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    $form['example'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('ict_gtfs.settings')->get('example'),
+    $form['base_url'] = [
+      '#type' => 'url',
+      '#title' => $this->t('URL for the GTFS server'),
+      '#default_value' => $this->config('ict_gtfs.settings')->get('base_url'),
     ];
+    $form['max_age'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Maximum cache age'),
+      '#min' => 0,
+      '#default_value' => $this->config('ict_gtfs.settings')->get('max_age'),
+    ];
+    $form['allowed_types'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Allowed GTFS types'),
+      '#description' => $this->t('Enter one per line.'),
+      '#default_value' => implode(PHP_EOL, $this->config('ict_gtfs.settings')->get('allowed_types')),
+    ];
+    $form['json_from_object'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Get JSON from GTFS object'),
+      '#description' => $this->t('If selected, then fetch binary data from the GTFS server and convert to JSON. Otherwise, fetch JSON from the server using the debug parameter. This setting has no effect when using GTFS objects instead of PHP arrays.'),
+      '#default_value' => $this->config('ict_gtfs.settings')->get('json_from_object') ?? TRUE,
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state): void {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $allowed_types = array_values(array_filter(array_map('trim',
+      explode("\n", $form_state->getValue('allowed_types'))
+    )));
+
     $this->config('ict_gtfs.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('base_url', $form_state->getValue('base_url'))
+      ->set('max_age', $form_state->getValue('max_age'))
+      ->set('allowed_types', $allowed_types)
+      ->set('json_from_object', $form_state->getValue('json_from_object'))
       ->save();
+
     parent::submitForm($form, $form_state);
   }
 
