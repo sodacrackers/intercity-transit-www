4ad86efccf06b4bfd554f9b5539ce357e3e8ad4a
Andrea Cividini <andrea.cividini@fruition.net>
2023-03-22 13:11:41 +0100

    Test commit to see if the automatic build is working

--- web/modules/custom/ict_routes_react_app/js/src/index.jsx
+++ web/modules/custom/ict_routes_react_app/js/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 // # Example 1: Simple "Hello, World" code
 ReactDOM.render(
-    <h1>This is React!</h1>,
+    <h1>This is a React app in Drupal!</h1>,
     document.getElementById('ict-routes-react-app')
 );
