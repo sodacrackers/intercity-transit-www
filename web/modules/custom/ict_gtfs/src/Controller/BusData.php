30fb981bb2d88da64813c4adbfa0266ae161b1c3
Andrea Cividini <andrea.cividini@fruition.net>
2023-03-27 16:09:58 +0200

    ICTROUTES-43: Caching improvements on the bus data endpoint

--- web/modules/custom/ict_gtfs/src/Controller/BusData.php
+++ web/modules/custom/ict_gtfs/src/Controller/BusData.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\ict_gtfs\Controller;
 
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\ict_gtfs\Gtfs;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Provides route responses for the Example module.
@@ -20,13 +23,23 @@ class BusData extends ControllerBase {
     private $gtfs;
 
     /**
+     * The renderer service.
+     *
+     * @var \Drupal\Core\Render\RendererInterface
+     */
+    private RendererInterface $renderer;
+
+    /**
      * BusData constructor.
      *
-     * @param \Drupal\Core\Form\FormBuilder $form_builder
-     *   The form builder.
+     * @param \Drupal\Core\Form\FormBuilder $gtfs
+     *   The gtfs service.
+     * @param \Drupal\Core\Render\RendererInterface $renderer
+     *   The renderer service.
      */
-    public function __construct(Gtfs $gtfs) {
+    public function __construct(Gtfs $gtfs, RendererInterface $renderer) {
         $this->gtfs = $gtfs;
+        $this->renderer = $renderer;
     }
 
     /**
@@ -34,15 +47,28 @@ class BusData extends ControllerBase {
      */
     public static function create(ContainerInterface $container) {
         return new static(
-            $container->get('ict.gtfs')
+            $container->get('ict.gtfs'),
+            $container->get('renderer')
         );
     }
 
     public function json() {
-        $payload = [];
-        foreach ($this->gtfs->getAllowedTypes() as $allowed_type) {
-            $payload[$allowed_type] = $this->gtfs->getArray($allowed_type);
-        }
-        return new JsonResponse($payload);
+        $context = new RenderContext();
+        $gtfs = $this->gtfs;
+        /** @var \Drupal\Core\Cache\CacheableJsonResponse $response */
+        $response = $this->renderer->executeInRenderContext($context, function () use ($gtfs) {
+            $payload = [];
+            foreach ($gtfs->getAllowedTypes() as $allowed_type) {
+                $payload[$allowed_type] = $gtfs->getArray($allowed_type);
+            }
+            $response = CacheableJsonResponse::create($payload);
+            $response->addCacheableDependency(CacheableMetadata::createFromRenderArray([
+                '#cache' => [
+                    'max-age' => 30,
+                ],
+            ]));
+            return $response;
+        });
+        return $response;
     }
 }
