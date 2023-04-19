b161a9d68b4e80cee2c4d04a3b884bf4d731737f
Edgar Harwood <edghar88@gmail.com>
2023-04-19 13:27:57 -0600

    initial commit -- data working

--- web/modules/custom/ict_routes_react_app/js/src/components/RealTimeArrivals/index.jsx
+++ web/modules/custom/ict_routes_react_app/js/src/components/RealTimeArrivals/index.jsx
@@ -3,87 +3,54 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
 import { DateTime } from 'luxon';
+import styles from './index.module.css';
 
 const RealTimeArrivals = () => {
   const [tripUpdates, setTripUpdates] = React.useState({});
   const [activeRoutes, setActiveRoutes] = React.useState([]);
   const [selectedRoute, setSelectedRoute] = React.useState(null);
-  const [selectedRouteData, setSelectedRouteData] = React.useState(null);
+  const [data, setData] = React.useState({});
+  const [sanitizedData, setSanitizedData] = React.useState({});
   const [direction, setDirection] = React.useState('inbound');
   const [view, setView] = React.useState('wait');
-  const [stopResults, setStopResults] = React.useState({});
+  const [loading, setLoading] = React.useState({});
 
-  const getData = async() => {
+  const getData = async(apiUrl) => {
     try {
-      const data = await fetch('https://localhost:49815/api/json/real-time-data', {
+      const data = await fetch(`${window.location.origin}${apiUrl}`, {
         headers: {
           'Access-Control-Allow-Origin': '*',
         }
       });
       const json = await data.json();
-      setTripUpdates(json.TripUpdate.entity);
+      const clean = {};
+      json.trips[direction].forEach((trip) => {
+        trip.stopTimes.forEach((st) => {
+          clean[st.stopSequence] = clean[st.stopSequence] ? [...clean[st.stopSequence], st] : [st];
+        })
+      })
+      setData(json);
+      console.log(clean);
+      setSanitizedData(clean);
+      setLoading(false);
     } catch (err) {
       console.log(err);
     }
-    return null;
   }
 
   React.useEffect(() => {
-    const results = {};
-    selectedRouteData?.forEach((item) => {
-      item.stopTimeUpdate.forEach((stop) => {
-        const index = Object.keys(results).indexOf(`Stop ${stop.stopId}`);
-        if (index > -1) {
-          results[`Stop ${stop.stopId}`].push({ vehicle: item.vehicle?.id, arrival: stop.arrival?.time, departure: stop.departure?.time  });
-          results[`Stop ${stop.stopId}`].sort((a, b) => DateTime.fromMillis(Number(a.arrival * 1000 < DateTime.fromMillis(Number(b.arrival * 1000)))));
-        } else {
-          results[`Stop ${stop.stopId}`] = [{ vehicle: item.vehicle?.id, arrival: stop.arrival?.time, departure: stop.departure?.time  }];
-        }
-        setStopResults((currentResults) => ({...currentResults, ...results}));
-      })
-    });
-  }, [selectedRouteData])
+    const apiUrl = document.getElementById('ict-routes-react-app').dataset.apiUrl;
+    setLoading(true);
+    getData(apiUrl);
+  }, [direction])
 
-  React.useEffect(() => {
-    const routes = Object.keys(tripUpdates).map((key) => {
-      return Number(tripUpdates[key].tripUpdate?.trip.routeId);
-    })
-    const activeRoutesCollection = [...new Set(routes)];
-    setActiveRoutes(activeRoutesCollection);
-  }, [tripUpdates])
-
-  React.useEffect(() => {
-    const activeBuses = Object.keys(tripUpdates).filter((key) => {
-      if (Number(tripUpdates[key].tripUpdate?.trip?.routeId) === selectedRoute) {
-        return key;
-      }
-    })
-    const data = [];
-    activeBuses.forEach((bus) => {
-      data.push(tripUpdates[bus].tripUpdate);
-    })
-    setSelectedRouteData(data);
-  }, [selectedRoute])
-
-  React.useEffect(() => {
-    getData();
-  }, [])
-
-  return (
-    <Container>
+  return Object.keys(data).length && !loading ? (
+    <Container className={styles.routesWrapper}>
       <Row>
-        {selectedRoute && (<div className="text-center">There are <strong>{selectedRouteData.length} buses</strong> currently on this route. Data is updated in real time.</div>)}
         <Col sm="9">
           <Row>
-            <Col>
-              <Form.Select onChange={e => setSelectedRoute(Number(e.target.value))}>
-                <option value={null}>Please select a route...</option>            
-                {activeRoutes.map((route) => (
-                  <option value={route}>Route {route}</option>
-                ))}
-              </Form.Select>
-            </Col>
             <Col>
               <Form.Check
                 type="radio"
@@ -117,33 +84,16 @@ const RealTimeArrivals = () => {
               />
             </Col>
             <Col xs="12">
-              {Object.keys(stopResults)?.map((_, index) => {
-                const positiveDiff = stopResults[Object.keys(stopResults)[index]]?.filter((stat) => {
-                  const now = DateTime.local();
-                  const upcoming =  DateTime.fromMillis(Number(stat.arrival * 1000));
-                  const diff = (now.diff(upcoming, 'minutes').toObject());
-                  return diff.minutes?.toFixed(0) > 0;
-                }).length > 0;
-                return positiveDiff && (
-                  <>
-                    <div className="d-inline-block">{Object.keys(stopResults)[index]}</div>
-                    {stopResults[Object.keys(stopResults)[index]]?.map((stopStat) => {
-                      const newNow = DateTime.local();
-                      const newUpcoming =  DateTime.fromMillis(Number(stopStat.arrival * 1000));
-                      const difference = (newNow.diff(newUpcoming, 'minutes').toObject());
-                      return (
-                        <div>
-                          {difference.minutes?.toFixed(0) > 0 && (
-                            <>
-                              <div className="d-inline-block mx-5">{`${difference.minutes?.toFixed(0)} Minutes`}</div>
-                            </>
-                          )}
-                        </div>
-                      )
-                    })}
-                  </>
-                )
-              })}
+              {Object.keys(sanitizedData)?.map((stop, stopIndex) => (
+                <Row>
+                  <Col md="3" key={`stopName-${stopIndex}`}>
+                    {sanitizedData[stop][0].stopName}
+                  </Col>
+                  <Col md="9">
+                    {sanitizedData[stop]?.map((item) => item.departureTime)}
+                  </Col>
+                </Row>
+              ))}
             </Col>
           </Row>
         </Col>
@@ -152,7 +102,7 @@ const RealTimeArrivals = () => {
         </Col>
       </Row>
     </Container>
-  );
+  ) : <div className="mt-5 text-center"><Spinner variant="dark" /></div>
 }
 
 export default RealTimeArrivals;
