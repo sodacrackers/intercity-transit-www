05a840855f8633afa94ef11edbfd54c3bfeaa019
Andrea Cividini <andrea.cividini@fruition.net>
2023-05-17 21:14:35 +0200

    More Tabs improvements

--- web/modules/custom/it_route_trip_tools/assets/js/route.js
+++ web/modules/custom/it_route_trip_tools/assets/js/route.js
@@ -1,17 +1,28 @@
 (function ($, Drupal, drupalSettings) {
   'use strict';
+  let has_loaded= false;
   Drupal.behaviors.route_tabs = {
     attach(context, settings) {
+      $('#real-time-container').hide();
       $('#realtime-tab', context).once('route-tab').click(function () {
         $('#schedule').hide();
-        let scriptEle = document.createElement("script");
+        $('#real-time-container').show();
+        if (!has_loaded) {
+          let scriptEle = document.createElement("script");
 
-        scriptEle.setAttribute("src", '/modules/custom/ict_routes_react_app/js/dist/main.min.js');
-        scriptEle.setAttribute("type", "text/javascript");
-        scriptEle.setAttribute("async", true);
+          scriptEle.setAttribute("src", '/modules/custom/ict_routes_react_app/js/dist/main.min.js');
+          scriptEle.setAttribute("type", "text/javascript");
+          scriptEle.setAttribute("async", true);
 
-        document.body.appendChild(scriptEle);
-      })
+          document.body.appendChild(scriptEle);
+          has_loaded = true;
+        }
+
+      });
+      $('#schedule-tab', context).once('schedule-tab').click(function () {
+        $('#schedule').show();
+        $('#real-time-container').hide();
+      });
     }
   };
 
