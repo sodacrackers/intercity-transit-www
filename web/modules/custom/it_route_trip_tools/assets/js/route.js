692f820948ffc9ab513ab363f20112bab96991fd
Andrea Cividini <andrea.cividini@fruition.net>
2023-06-20 16:33:50 +0200

    Style and loading updates

--- web/modules/custom/it_route_trip_tools/assets/js/route.js
+++ web/modules/custom/it_route_trip_tools/assets/js/route.js
@@ -3,20 +3,20 @@
   let has_loaded= false;
   Drupal.behaviors.route_tabs = {
     attach(context, settings) {
+      if (!has_loaded) {
+        let scriptEle = document.createElement("script");
+
+        scriptEle.setAttribute("src", '/modules/custom/ict_routes_react_app/js/dist/main.min.js');
+        scriptEle.setAttribute("type", "text/javascript");
+        scriptEle.setAttribute("async", true);
+
+        document.body.appendChild(scriptEle);
+        has_loaded = true;
+      }
       $('#real-time-container').hide();
       $('#realtime-tab', context).once('route-tab').click(function () {
         $('#schedule').hide();
         $('#real-time-container').show();
-        if (!has_loaded) {
-          let scriptEle = document.createElement("script");
-
-          scriptEle.setAttribute("src", '/modules/custom/ict_routes_react_app/js/dist/main.min.js');
-          scriptEle.setAttribute("type", "text/javascript");
-          scriptEle.setAttribute("async", true);
-
-          document.body.appendChild(scriptEle);
-          has_loaded = true;
-        }
 
       });
       $('#schedule-tab', context).once('schedule-tab').click(function () {
