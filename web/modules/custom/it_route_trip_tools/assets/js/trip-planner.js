1f004d4f1286c9ebc5a8de5f03e2ea34195ceb86
Hung Do Manh <hudo.do.manh@fruition.net>
2023-08-25 12:10:14 -0400

    ICTSD-58: Show select label and enable/disable button when the stop has been selected.

--- web/modules/custom/it_route_trip_tools/assets/js/trip-planner.js
+++ web/modules/custom/it_route_trip_tools/assets/js/trip-planner.js
@@ -7,15 +7,15 @@
         //Adds timepicker dropdown for Trip Planner and Find a Route time fields
         $('#time').timepicker();
         $('#time').timepicker('option', {'useSelect': true, 'timeFormat': 'g:i a', 'setTime': Date.now(), 'step': 5, 'className': 'form-control timepicker-content'});
-        
+
         $('#route-time').timepicker();
         $('#route-time').timepicker('option', {'useSelect': true, 'timeFormat': 'g:i a', 'setTime': Date.now(), 'step': 5, 'className': 'form-control timepicker-content'});
-        
+
 
         //Adds jQuery Datepicker function to Trip Planner and Find a Route date fields
         $('input#date').datepicker();
         $('input#date').datepicker('setDate', new Date());
-        
+
         //Adds time 1 and 2 IDs to the time input fields. The timepicker tool strips these tags away so they have to be replaced.
         $('#timepicker-content').attr('id', 'time').attr('name', 'time');
         $('#timepicker-menu').attr('id', 'time1').attr('name', 'time');
@@ -24,6 +24,21 @@
             $(clickedClass).each(function(){
                 $(this).val('');
             });
+        });
+
+        var $ = jQuery;
+        var stop_selected = $('#stops-form option:selected').val();
+        stop_selected = stop_selected && stop_selected != 'Select a stop';
+        $('#stops-form button').prop('disabled', stop_selected ? false : true);
+        $('#stops-form select').change(function() {
+            var val = $(this).find(":selected").val();
+            val = val && val != 'Select a stop';
+            if (val) {
+                $('#stops-form button').prop('disabled', false);
+            }
+            else {
+                $('#stops-form button').prop('disabled', true);
+            }
         });
     });
 })(jQuery);
