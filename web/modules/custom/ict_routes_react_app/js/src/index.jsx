aae63050aeb599f5f46ac932cac9e2a8549a20dc
Edgar Harwood <edghar88@gmail.com>
2023-10-13 09:19:21 -0600

    inital commit - WIP

--- web/modules/custom/ict_routes_react_app/js/src/index.jsx
+++ web/modules/custom/ict_routes_react_app/js/src/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import RealTimeArrivals from './components/RealTimeArrivals';
+import RealTimeArrivals from './components/RealTimeArrivals/index-new';
 import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
