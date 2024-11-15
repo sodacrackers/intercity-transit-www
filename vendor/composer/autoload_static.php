875a83c5004bf6955d5b11bab88ae5b1f90fabfa
Andrea Cividini <andrea.cividini@fruition.net>
2024-11-15 11:25:41 +0100

    ICTSD-67: Another round of config export

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit69688ecc3bf36106f3798e86e44237fb
+class ComposerStaticInit7af88cb9049689115a8b415aa6dbca9f
 {
     public static $files = array (
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
@@ -1723,10 +1723,10 @@ class ComposerStaticInit69688ecc3bf36106f3798e86e44237fb
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit69688ecc3bf36106f3798e86e44237fb::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit69688ecc3bf36106f3798e86e44237fb::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit69688ecc3bf36106f3798e86e44237fb::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit69688ecc3bf36106f3798e86e44237fb::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit7af88cb9049689115a8b415aa6dbca9f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit7af88cb9049689115a8b415aa6dbca9f::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit7af88cb9049689115a8b415aa6dbca9f::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit7af88cb9049689115a8b415aa6dbca9f::$classMap;
 
         }, null, ClassLoader::class);
     }
