43859ab87889d6ef3d88f1d5a076c5acfd61f9a9
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2025-10-02 11:35:47 -0700

    Trigger build.

--- web/private/scripts/github-push.php
+++ web/private/scripts/github-push.php
@@ -16,20 +16,12 @@ echo "== Starting Quicksilver GitHub Push Script =="
 cd "\$HOME/code"
 
 TOKEN=\$(cat \$HOME/files/private/prod-workflow-ict-github-token.txt)
-echo "Token preview: \${TOKEN:0:10}..."
+echo "Token preview: \${TOKEN:0:30}..."
 
-GITHUB_URL="https://\${TOKEN}@github.com/Intercity-Transit/intercity-transit-www.git"
-
-if ! git remote | grep -q transitgithub; then
-  echo "Adding remote"
-  git remote add transitgithub "\$GITHUB_URL"
-else
-  echo "Updating remote to use latest token"
-  git remote set-url transitgithub "\$GITHUB_URL"
-fi
+GITHUB_URL="https://${TOKEN}@github.com/Intercity-Transit/intercity-transit-www.git"
 
 echo "Pushing Pantheon 'master' branch to GitHub 'master' branch"
-git push transitgithub master:master --force 2>&1
+git push "$GITHUB_URL" master:master --force 2>&1
 
 echo "== Done =="
 BASH;
